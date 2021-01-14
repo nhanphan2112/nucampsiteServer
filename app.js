5e4c304dab93597e341bec980274c9c3a6bdad03
Nhan Phan <nphanacc@gmail.com>
2021-01-13 19:59:55 -0800

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -34,14 +36,25 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321"));
+// app.use(cookieParser("12345-67890-09876-54321")); replace by session cause session often has their own cookie
+
+app.use(
+	session({
+		name: "session-id",
+		secret: "12345-67890-09876-54321",
+		saveUninitialized: false,
+		resave: false,
+		store: new FileStore(),
+	})
+);
 
 function auth(req, res, next) {
-	// set up cookie to talk to server from client machine
-	if (!req.signedCookies.user) {
+	console.log(req.session);
+
+	if (!req.session.user) {
 		const authHeader = req.headers.authorization;
 		if (!authHeader) {
-			const err = new Error("You are not authenticated");
+			const err = new Error("You are not authenticated!");
 			res.setHeader("WWW-Authenticate", "Basic");
 			err.status = 401;
 			return next(err);
@@ -51,8 +64,8 @@ function auth(req, res, next) {
 		const user = auth[0];
 		const pass = auth[1];
 		if (user === "admin" && pass === "password") {
-			res.cookie("user", "admin", { signed: true });
-			return next(); //authorized
+			req.session.user = "admin";
+			return next(); // authorized
 		} else {
 			const err = new Error("You are not authenticated!");
 			res.setHeader("WWW-Authenticate", "Basic");
@@ -60,7 +73,7 @@ function auth(req, res, next) {
 			return next(err);
 		}
 	} else {
-		if ((req.signedCookies.user = "admin")) {
+		if (req.session.user === "admin") {
 			return next();
 		} else {
 			const err = new Error("You are not authenticated!");
